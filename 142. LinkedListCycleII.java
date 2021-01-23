b8da1b570c0b0eb6c98e7d4866c2d1bf32824bc4
SmilingWayne <69518108+SmilingWayne@users.noreply.github.com>
2021-01-23 13:51:55 +0800

    Update and rename 142. Linked List Cycle II to 142. LinkedListCycleII.java
    双指针进阶版。首先找到开始循环的节点。

--- 142. LinkedListCycleII.java
+++ 142. LinkedListCycleII.java
@@ -14,7 +14,7 @@
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
-Java.Version1: //Very Important//
+//Java.Version1: Very Important//
 
 public class Solution {
     public ListNode detectCycle(ListNode head) {
@@ -44,7 +44,7 @@ public class Solution {
     }
 }
 
-Java.Version2:
+//Java.Version2:
 
 /**
  * Definition for singly-linked list.
